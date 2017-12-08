1bb035c83af1fd465f7f6970e616d935c55b1b97
Julian <weiyi.li713@gmail.com>
2017-12-08 14:32:16 +1100

    contact form

---
+++ src/Contact/Contact.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Contact;
+
+class Contact
+{
+    private $name;
+    private $email;
+    private $subject;
+    private $message;
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+
+    /**
+     * @param mixed $subject
+     */
+    public function setSubject($subject): void
+    {
+        $this->subject = $subject;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param mixed $message
+     */
+    public function setMessage($message): void
+    {
+        $this->message = $message;
+    }
+}
--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,22 +2,38 @@
 
 namespace App\Controller;
 
+use App\Contact\Contact;
 use App\Entity\Freelancer;
+use App\Form\ContactType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class ProfileController extends AbstractController
 {
     /**
      * @Route("/", name="index")
      */
-    public function index()
+    public function index(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
 
         $repo = $em->getRepository(Freelancer::class);
         $freelancer = $repo->findFreeLancer();
 
-        return $this->render('profile.html.twig', array('freelancer' => $freelancer));
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contact = $form->getData();
+            return $this->redirectToRoute('index', ['_fragment'=>'contact']);
+        }
+
+        return $this->render('profile.html.twig', array(
+            'freelancer' => $freelancer,
+            'form' => $form->createView()
+        ));
     }
 }
