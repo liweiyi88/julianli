7ab8bfedc3979c3f9a9c1a16ff0df72e719fff52
Julian <weiyi.li713@gmail.com>
2017-12-09 21:06:33 +1100

    contact api

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class BaseController extends AbstractController
+{
+    private $serializer;
+
+    public function __construct(SerializerInterface $serializer)
+    {
+        $this->serializer = $serializer;
+    }
+
+    protected function createApiResponse($data, $statusCode = 200)
+    {
+        $json = $this->serialize($data);
+
+        return new Response($json, $statusCode, array(
+            'Content-Type' => 'application/json'
+        ));
+    }
+
+    protected function serialize($data, $format = 'json')
+    {
+        return $this->serializer->serialize($data, $format);
+    }
+}
--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -3,42 +3,94 @@
 namespace App\Controller;
 
 use App\Contact\Contact;
+use App\Api\ApiProblem;
+use App\Api\ApiProblemException;
 use App\Email\EmailManager;
 use App\Entity\Freelancer;
 use App\Form\ContactType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 
-class ProfileController extends AbstractController
+class ProfileController extends BaseController
 {
     /**
      * @Route("/", name="index")
      */
-    public function index(Request $request, EmailManager $emailManager)
+    public function index(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
 
         $repo = $em->getRepository(Freelancer::class);
         $freelancer = $repo->findFreeLancer();
 
+        return $this->render('profile.html.twig', array(
+            'freelancer' => $freelancer
+        ));
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function postContact(Request $request, EmailManager $emailManager)
+    {
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact);
+        $this->processForm($request, $form);
 
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $contact = $form->getData();
-
-            $message = $emailManager->createPlianMessageFromContact($contact);
-            $emailManager->sendEmail($message);
-
-            return $this->redirectToRoute('index', ['_fragment'=>'contact']);
+        if (!$form->isValid()) {
+            $this->throwApiProblemValidationException($form);
         }
 
-        return $this->render('profile.html.twig', array(
-            'freelancer' => $freelancer,
-            'form' => $form->createView()
-        ));
+        $contact = $form->getData();
+        $message = $emailManager->createPlianMessageFromContact($contact);
+        $emailManager->sendEmail($message);
+
+        $response = $this->createApiResponse($contact, 200);
+        return $response;
+    }
+
+    private function processForm(Request $request, FormInterface $form)
+    {
+        $data = json_decode($request->getContent(), true);
+        if ($data === null) {
+            $apiProblem = new ApiProblem(400, ApiProblem::TYPE_INVALID_REQUEST_BODY_FORMAT);
+
+            throw new ApiProblemException($apiProblem);
+        }
+
+        $clearMissing = $request->getMethod() != 'PATCH';
+        $form->submit($data, $clearMissing);
+    }
+
+    private function getErrorsFromForm(FormInterface $form)
+    {
+        $errors = array();
+        foreach ($form->getErrors() as $error) {
+            $errors[] = $error->getMessage();
+        }
+
+        foreach ($form->all() as $childForm) {
+            if ($childForm instanceof FormInterface) {
+                if ($childErrors = $this->getErrorsFromForm($childForm)) {
+                    $errors[$childForm->getName()] = $childErrors;
+                }
+            }
+        }
+
+        return $errors;
+    }
+
+    private function throwApiProblemValidationException(FormInterface $form)
+    {
+        $errors = $this->getErrorsFromForm($form);
+
+        $apiProblem = new ApiProblem(
+            400,
+            ApiProblem::TYPE_VALIDATION_ERROR
+        );
+        $apiProblem->set('errors', $errors);
+
+        throw new ApiProblemException($apiProblem);
     }
 }
