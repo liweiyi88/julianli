5899ecd7a99dd9364d807f33df948253a15528e5
Julian Li <weiyi.li713@gmail.com>
2018-03-31 21:36:04 +1100

    ckeditor 5

---
+++ public/js/ckeditorcustom.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+    document.querySelectorAll( '.awesome-ckeditor textarea' )
+        .forEach(function(el){
+            el.removeAttribute('required');
+            ClassicEditor
+                .create( el , {
+                    ckfinder: {
+                        uploadUrl: '/api/uploads'
+                    }
+                })
+                .then( function (editor) {
+                    console.log( editor );
+                    var div = el.parentNode.querySelector('.ck-editor__editable');
+                    div.style.backgroundColor = 'white';
+                    div.style.minHeight = '300px';
+                } )
+                .catch( function (error) {
+                    console.error( error );
+                } );
+        });
+
+});
