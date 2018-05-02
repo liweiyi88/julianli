7e540ee3ebe78c0b549e4b8a2be4249610130875
Julian Li <liweiyi88@users.noreply.github.com>
2018-05-02 16:52:43 +1000

    Tests (#4)
    * init test commit
    
    * tests
    
    * update packages
    
    * using ckeditor 4 instead of 5

--- public/js/ckeditorcustom.js
+++ public/js/ckeditorcustom.js
@@ -1,23 +1,3 @@
 $(document).ready(function () {
-
-    document.querySelectorAll( '.awesome-ckeditor textarea' )
-        .forEach(function(el){
-            el.removeAttribute('required');
-            ClassicEditor
-                .create( el , {
-                    ckfinder: {
-                        uploadUrl: '/file/upload'
-                    }
-                })
-                .then( function (editor) {
-                    console.log( editor );
-                    var div = el.parentNode.querySelector('.ck-editor__editable');
-                    div.style.backgroundColor = 'white';
-                    div.style.minHeight = '300px';
-                } )
-                .catch( function (error) {
-                    console.error( error );
-                } );
-        });
-
+    CKEDITOR.replace( 'post_content' );
 });
---
+++ tests/Service/S3Test.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Service;
+
+use PHPUnit\Framework\TestCase;
+
+class S3Test extends TestCase
+{
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+    }
+}
