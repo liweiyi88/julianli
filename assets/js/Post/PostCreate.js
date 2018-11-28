f2b43ac6436c912c04f62a8dffc4b03548f0405f
Julian Li <weiyi.li713@gmail.com>
2018-11-28 15:55:56 +1100

    refactoring code

--- assets/js/Post/PostCreate.js
+++ assets/js/Post/PostCreate.js
@@ -1,18 +1,65 @@
 import React, {Component} from 'react';
 import PostForm from './PostForm';
+import SimpleMDE from "simplemde";
 
 export default class PostCreate extends Component
 {
     constructor(props) {
         super(props);
+
+        this.state = {
+            title: '',
+            slug: '',
+            content: '',
+            selectedTags: null,
+            isPublished: null,
+            isPublic: true
+        };
+
+        this.handleFormElementChange = this.handleFormElementChange.bind(this);
+        this.handleTagsSelectChange = this.handleTagsSelectChange.bind(this);
+    }
+
+    componentDidMount() {
+        const simplemde = new SimpleMDE({ element: document.getElementById('content') });
+
+        simplemde.codemirror.on('change', () =>{
+            this.setState({
+                content: simplemde.value()
+            });
+        });
+    }
+
+    handleTagsSelectChange(selectedOption) {
+        this.setState({ selectedTags: selectedOption });
+    }
+
+    handleFormElementChange(event) {
+        const target = event.target;
+
+        this.setState({
+            [target.name]: target.type === 'checkbox'
+                ? target.checked
+                : target.value
+        });
     }
 
     render() {
+        const tags = [
+            {value:1, label:'Life style'},
+            {value:2, label:'Symfony'},
+            {value:3, label:'Tech'},
+            {value:4, label:'Security'}
+        ];
+
         return (
             <div className={`container mx-auto w-3/4`}>
                 <div className={`flex items-center mt-10 mb-4 justify-center md:flex md:items-center mb-6`}>
                     <PostForm
                         {...this.state}
+                        tags={tags}
+                        onElementChange={this.handleFormElementChange}
+                        onTagsSelectedChange={this.handleTagsSelectChange}
                     />
                 </div>
             </div>
--- assets/js/Post/PostForm.js
+++ assets/js/Post/PostForm.js
@@ -1,148 +1,97 @@
 import React from 'react';
-import {Component} from 'react';
 import PropTypes from 'prop-types';
 import CreatableSelect from 'react-select/lib/Creatable';
-import SimpleMDE from 'simplemde';
 import Toggle from './Toggle';
 
-export default class PostForm extends Component {
-    constructor(props) {
-        super(props);
+export default function PostForm(props) {
+    const {
+        title,
+        slug,
+        content,
+        isPublic,
+        tags,
+        onTagsSelectedChange,
+        onElementChange,
+        selectedTags
+    } = props;
 
-        this.state = {
-            id: '',
-            title: '',
-            slug: '',
-            content: '',
-            selectedTags: null,
-            published: null,
-            public: true
-        };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleTagsSelectChange = this.handleTagsSelectChange.bind(this);
-    }
-
-    componentDidMount() {
-        const simplemde = new SimpleMDE({ element: document.getElementById('content') });
-
-        simplemde.codemirror.on('change', () =>{
-            this.setState({
-                content: simplemde.value()
-            });
-        });
-    }
-
-    handleTagsSelectChange(selectedOption) {
-        this.setState({ selectedTags: selectedOption });
-        console.log(`Option selected:`, selectedOption);
-    }
-
-    handleChange(event) {
-        const target = event.target;
-        console.log(target);
-
-        this.setState({
-            [target.name]: target.type === 'checkbox'
-                ? target.checked
-                : target.value
-        });
-    }
-
-    hasEditingPost(editingPost) {
-        if (editingPost === undefined) {
-            return false;
-        }
-
-        return Object.getOwnPropertyNames(editingPost).length > 0;
-    }
-
-    render() {
-        const {title, slug, content} = this.state;
-        const {editingPost} = this.props;
-        
-        const tags = [
-            {value:1, label:'Life style'},
-            {value:2, label:'Symfony'},
-            {value:3, label:'Tech'},
-            {value:4, label:'Security'}
-        ];
-
-        const toggle = this.hasEditingPost(editingPost) ? editingPost.public : true;
-
-        return (
-            <div className={`w-full`}>
-                <div className={`flex flex-row-reverse`}>
-                    <div><button className={`bg-transparent hover:bg-green text-green-dark hover:text-white py-2 px-4 border border-green hover:border-transparent rounded`}>Ready to publish?</button></div>
-                    <div className={`flex items-center mr-4`}>
-                        <Toggle
-                            toggle={toggle}
-                            toggleOnText={`It is now public`}
-                            toggleOnTextColor={`text-green`}
-                            toggleOffText={`It is now private`}
-                            toggleOffTextColor={`text-orange`}
-                        />
-                    </div>
+    return (
+        <div className={`w-full`}>
+            <div className={`flex flex-row-reverse`}>
+                <div>
+                    <button
+                        className={`bg-transparent hover:bg-green text-green-dark hover:text-white py-2 px-4 border border-green hover:border-transparent rounded`}>Ready
+                        to publish?
+                    </button>
                 </div>
-                { this.hasEditingPost(editingPost) > 0 && (<div>Id: {editingPost.id}</div>)}
-                <div className={`mt-5`}>
-                    <input
-                        className={`form-input focus:outline-none focus:bg-white focus:border-blue`}
-                        onChange={this.handleChange}
-                        type='text'
-                        name='title'
-                        id='post_title'
-                        placeholder={`Title`}
-                        value={this.hasEditingPost(editingPost) ? editingPost.title : title}
+                <div className={`flex items-center mr-4`}>
+                    <Toggle
+                        toggle={isPublic}
+                        toggleOnText={`It is now public`}
+                        toggleOnTextColor={`text-green`}
+                        toggleOffText={`It is now private`}
+                        toggleOffTextColor={`text-orange`}
                     />
                 </div>
-
-                <div className={`mt-5`}>
-                    <input
-                        className={`form-input focus:outline-none focus:bg-white focus:border-blue`}
-                        placeholder={`Slug`}
-                        onChange={this.handleChange}
-                        type='text' name='slug'
-                        id='post_slug'
-                        value={this.hasEditingPost(editingPost) ? editingPost.slug : slug}
-                    />
-                </div>
-
-                <div className={`mt-5`}>
-                    <CreatableSelect
-                        theme={(theme) => ({
-                            ...theme,
-                            spacing: {
-                                ...theme.spacing,
-                                controlHeight: '3rem'
-                            }
-                        })}
-
-                        value={this.state.selectedTags}
-                        options={tags}
-                        onChange={this.handleTagsSelectChange}
-                        isMulti
-                        placeholder={`Add a tags..`}
-                        isValidNewOption={(inputValue, selectValue, selectOptions) => {
-                            const isNotDuplicated = !selectOptions
-                                .map(option => option.label)
-                                .includes(inputValue);
-                            const isNotEmpty = inputValue !== '';
-                            return isNotEmpty && isNotDuplicated;
-                        }}
-                    />
-                </div>
-
-                <div className={`mt-5`}>
-                    <textarea onChange={this.handleChange} id='content' name='content' value={this.hasEditingPost(editingPost) ? editingPost.content : content}/>
-                </div>
-
-
             </div>
-        );
-    }
+
+            <div className={`mt-5`}>
+                <input
+                    className={`form-input focus:outline-none focus:bg-white focus:border-blue`}
+                    onChange={onElementChange}
+                    type='text'
+                    name='title'
+                    id='post_title'
+                    placeholder={`Title`}
+                    value={title}
+                />
+            </div>
+
+            <div className={`mt-5`}>
+                <input
+                    className={`form-input focus:outline-none focus:bg-white focus:border-blue`}
+                    placeholder={`Slug`}
+                    onChange={onElementChange}
+                    type='text' name='slug'
+                    id='post_slug'
+                    value={slug}
+                />
+            </div>
+
+            <div className={`mt-5`}>
+                <CreatableSelect
+                    theme={(theme) => ({
+                        ...theme,
+                        spacing: {
+                            ...theme.spacing,
+                            controlHeight: '3rem'
+                        }
+                    })}
+
+                    value={selectedTags}
+                    options={tags}
+                    onChange={onTagsSelectedChange}
+                    isMulti
+                    placeholder={`Add a tags..`}
+                    isValidNewOption={(inputValue, selectValue, selectOptions) => {
+                        const isNotDuplicated = !selectOptions
+                            .map(option => option.label)
+                            .includes(inputValue);
+                        const isNotEmpty = inputValue !== '';
+                        return isNotEmpty && isNotDuplicated;
+                    }}
+                />
+            </div>
+
+            <div className={`mt-5`}>
+                <textarea onChange={onElementChange} id='content' name='content' value={content}/>
+            </div>
+        </div>
+    );
 }
 
 PostForm.propTypes = {
-    editingPost: PropTypes.object
+    tags: PropTypes.array.isRequired,
+    onElementChange: PropTypes.func.isRequired,
+    onTagsSelectedChange: PropTypes.func.isRequired
 };
