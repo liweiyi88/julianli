133294241036065119f0d1ffcd94b02ac3c2d2ca
liweiyi88 <weiyi.li713@gmail.com>
2019-01-02 21:41:52 +1100

    disable query on page load

--- assets/js/UtilComponent/Search.js
+++ assets/js/UtilComponent/Search.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import algoliasearch from 'algoliasearch/lite';
+import algoliasearch from 'algoliasearch';
 import { InstantSearch } from 'react-instantsearch-dom';
 import { SearchBox } from 'react-instantsearch-dom';
 import { Hits } from 'react-instantsearch-dom';
 
-const searchClient = algoliasearch(
-    '57OYAZ5QGF',
-    'd24d74854fd96fc71666fa57cad0c60e'
-);
+const algoliaClient = algoliasearch('57OYAZ5QGF', 'd24d74854fd96fc71666fa57cad0c60e', {
+    _useRequestCache: true,
+});
+
+const searchClient = {
+    search(requests) {
+        const shouldSearch = requests.some(( query ) =>  query['params']['query'] !== '' );
+
+        if (shouldSearch) {
+            return algoliaClient.search(requests);
+        }
+
+        return Promise.resolve({
+            results: [{ hits: [] }],
+        });
+    },
+    searchForFacetValues: algoliaClient.searchForFacetValues,
+};
 
 const Hit = ({ hit }) => <a href={'/admin/posts/'+ hit.objectID +'/show'}>{hit.title}</a>;
 
