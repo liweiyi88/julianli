e7ff798d8d1b85eca3227028dd9c6b7485c7bc94
liweiyi88 <weiyi.li713@gmail.com>
2019-01-03 17:10:30 +1100

    style improvements

--- assets/js/UtilComponent/Search.js
+++ assets/js/UtilComponent/Search.js
@@ -1,38 +1,65 @@
-import React from 'react';
+import React, {Component} from 'react';
 import algoliasearch from 'algoliasearch';
 import { InstantSearch } from 'react-instantsearch-dom';
 import { SearchBox } from 'react-instantsearch-dom';
 import { Hits } from 'react-instantsearch-dom';
 
-const algoliaClient = algoliasearch('57OYAZ5QGF', 'd24d74854fd96fc71666fa57cad0c60e', {
-    _useRequestCache: true,
-});
+export default class Search extends Component{
+    constructor(props) {
+        super(props);
 
-const searchClient = {
-    search(requests) {
-        const shouldSearch = requests.some(( query ) =>  query['params']['query'] !== '' );
+        this.state = {
+            showHits: false,
+        };
 
-        if (shouldSearch) {
-            return algoliaClient.search(requests);
+        this.handleSearchBoxType = this.handleSearchBoxType.bind(this);
+    }
+
+    handleSearchBoxType(event) {
+        if (event.target.value.trim() !== '') {
+            this.setState({
+                showHits: true
+            });
+
+            return;
         }
 
-        return Promise.resolve({
-            results: [{ hits: [] }],
+        this.setState({
+            showHits: false
         });
-    },
-    searchForFacetValues: algoliaClient.searchForFacetValues,
-};
+    }
 
-const Hit = ({ hit }) => <a href={'/admin/posts/'+ hit.objectID +'/show'}>{hit.title}</a>;
+    render() {
+        const algoliaClient = algoliasearch('57OYAZ5QGF', 'd24d74854fd96fc71666fa57cad0c60e', {
+            _useRequestCache: true,
+        });
 
-const Search = () => (
-    <InstantSearch
-        indexName='dev_posts'
-        searchClient={searchClient}
-    >
-        <SearchBox />
-        <Hits hitComponent={Hit} />
-    </InstantSearch>
-);
+        const searchClient = {
+            search(requests) {
+                const shouldSearch = requests.some(( query ) =>  query['params']['query'] !== '' );
 
-export default Search;
+                if (shouldSearch) {
+                    return algoliaClient.search(requests);
+                }
+
+                return Promise.resolve({
+                    results: [{ hits: [] }],
+                });
+            },
+            searchForFacetValues: algoliaClient.searchForFacetValues,
+        };
+
+        const Hit = ({ hit }) => <a href={'/admin/posts/'+ hit.objectID +'/show'}>{hit.title}</a>
+
+        return (
+            <InstantSearch
+                indexName='dev_posts'
+                searchClient={searchClient}
+            >
+                <SearchBox onChange={this.handleSearchBoxType}/>
+
+                { this.state.showHits && <Hits hitComponent={Hit} />}
+            </InstantSearch>
+        )
+    }
+}
