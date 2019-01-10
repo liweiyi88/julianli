3ae2018b47e324527fd49df301f991a5090009c6
liweiyi88 <weiyi.li713@gmail.com>
2019-01-10 15:59:03 +1100

    move hardcoded algolia search values into config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,13 @@
 var Encore = require('@symfony/webpack-encore');
 
 Encore
-// the project directory where compiled assets will be stored
     .setOutputPath('public/build/')
-    // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
-    // the public path you will use in Symfony's asset() function - e.g. asset('build/some_file.js')
     .setManifestKeyPrefix('build/')
 
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
 
-    // the following line enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
     .enableReactPreset()
     .configureBabel((babelConfig) => {
@@ -31,6 +27,10 @@ Encore
         resolveUrlLoader: false
     })
     .enablePostCssLoader()
+    .configureDefinePlugin((options) => {
+        options.ALGOLIA_SEARCH_ONLY_API_KEY = '0a297293561cc822339db9f46b0b3bd4';
+        options.ALGOLIA_APP_ID = '57OYAZ5QGF'
+    })
 ;
 
 if (Encore.isProduction()) {
