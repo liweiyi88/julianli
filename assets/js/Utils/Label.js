fdd5ee9b41d213c71978b61ac71e09248d4d7913
liweiyi88 <weiyi.li713@gmail.com>
2019-01-25 12:06:26 +1100

    ci fixes

--- assets/js/Utils/Label.js
+++ assets/js/Utils/Label.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 export default function Label(props) {
     return (
         <span className={'rounded-sm bg-grey-lighter text-grey-darker uppercase px-2 py-1 text-sm ml-3 cursor-pointer hover:bg-grey-light'}>{props.name}</span>
     )
+}
+
+Label.propTypes = {
+    name: PropTypes.string.isRequired
 }
