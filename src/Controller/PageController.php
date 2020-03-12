d58539453c4addd15f1eccd6623f9b0dbb1a8da0
Julian Li <weiyi.li713@gmail.com>
2020-03-12 11:25:13 +1100

    refactoring pages

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -6,18 +6,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class PageController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      * @Route("/articles", name="articles")
      * @Route("/article/using-symfony-messenger-with-aws-sqs", name="article_using_symfony_messenger_with_aws_sqs")
      * @Route("/article/keep-learning-in-2018", name="article_keep_learning_in_2018")
-     * @Route("/projects", name="projects")
      * @Route("/hire-me", name="hire_me")
      */
     public function index(): Response
     {
         return $this->render('home.html.twig');
+    }
+
+    /**
+     * @Route("/projects", name="projects")
+     */
+    public function projects(): Response
+    {
+        return $this->render('project.html.twig');
     }
 }
