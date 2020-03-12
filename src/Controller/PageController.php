1a6bdff5e4e2d3e6d807348ec50820653df97185
Julian Li <weiyi.li713@gmail.com>
2020-03-12 14:57:57 +1100

    article show page

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -19,8 +19,6 @@ class PageController extends AbstractController
 
     /**
      * @Route("/", name="home")
-     * @Route("/article/using-symfony-messenger-with-aws-sqs", name="article_using_symfony_messenger_with_aws_sqs")
-     * @Route("/article/keep-learning-in-2018", name="article_keep_learning_in_2018")
      * @Route("/hire-me", name="hire_me")
      */
     public function index(): Response
